e2ad9c0e8df8a957590355207f1b7cfc25e4e5d7
GuilhermeCunha <guiscunha@gmail.com>
2019-05-23 04:48:32 -0300

    Maps Matrix não funcional

--- app/src/main/java/com/example/savegas/MapsActivity.java
+++ app/src/main/java/com/example/savegas/MapsActivity.java
@@ -1,30 +1,47 @@
 package com.example.savegas;
 
 import android.Manifest;
+import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.matrix.v1.MapboxMatrix;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import static com.example.savegas.MapsDistanceMatrix.getDistanceInfo;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback ,
+        GoogleApiClient.OnConnectionFailedListener{
     private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
 
@@ -33,6 +50,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
 
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    private GoogleApiClient mGoogleApiClient;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136));
+    private MapsDistanceMatrix matrix;
+
+    //Elementos
+    //private AutoCompleteTextView acOrigem;
+    //private AutoCompleteTextView acDestino;
+    private EditText edOrigem;
+    private EditText edDestino;
+
+
+    private Button BotaoCalcular;
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
@@ -40,9 +74,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.getUiSettings().setZoomControlsEnabled(true);
         //LatLng llorigem = new LatLng();
         //LatLng lldestino = new LatLng();
-        MarkerOptions origem = new MarkerOptions();
         //origem.position(llorigem);
-        MarkerOptions destino = new MarkerOptions();
         //origem.position(lldestino);
 
         // Add a marker in Sydney and move the camera
@@ -50,6 +82,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng brasil = new LatLng(-12.500932, -39.189517);
         mMap.addMarker(new MarkerOptions().position(brasil).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(brasil));
+        init();
+    }
+
+    public void init(){
+        /*
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS, null);
+        acOrigem = (AutoCompleteTextView) findViewById(R.id.EditEnderecoOrigem);
+        acOrigem.setAdapter(mPlaceAutocompleteAdapter);
+        acDestino = (AutoCompleteTextView) findViewById(R.id.EditEnderecoDestino);
+        acDestino.setAdapter(mPlaceAutocompleteAdapter);
+        */
+
 
         Button botaoCalcular;
         botaoCalcular = (Button) findViewById(R.id.BotaoCalcularGastoPorTrajeto);
@@ -58,15 +108,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         botaoCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                edOrigem = (EditText) findViewById(R.id.EditEnderecoOrigem);
+                edDestino = (EditText) findViewById(R.id.EditEnderecoDestino);
                 Log.e("CRIADO", "Entrei pelo botao");
-                EditText origem;
-                EditText destino;
 
-                origem = (EditText) findViewById(R.id.EditEnderecoOrigem);
-                destino = (EditText) findViewById(R.id.EditEnderecoDestino);
 
-                String txtOrigem = origem.getText().toString();
-                String txtDestino = destino.getText().toString();
+                String txtOrigem = edOrigem.getText().toString();
+                String txtDestino = edDestino.getText().toString();
                 Log.e("CRIADO: " , txtOrigem);
                 Log.e("CRIADO: " , txtDestino);
 
@@ -83,9 +131,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Log.e("ENDEREÇO", adDestino.getAddressLine(0));
                     LatLng llOrigem = new LatLng(adOrigem.getLatitude(),adOrigem.getLongitude());
                     LatLng llDestino = new LatLng(adDestino.getLatitude(),adDestino.getLongitude());
-
+                    Location a = new Location("");
+                    a.setLatitude(llOrigem.latitude);
+                    a.setLongitude(llOrigem.longitude);
+                    Location b = new Location("");
+                    b.setLatitude(llDestino.latitude);
+                    b.setLongitude(llDestino.longitude);
+                    mMap.addMarker(new MarkerOptions().position(llOrigem).title(adOrigem.getAddressLine(0)));
+                    mMap.addMarker(new MarkerOptions().position(llDestino).title(adDestino.getAddressLine(0)));
+                    float distance = a.distanceTo(b)/1000;
+                    Toast.makeText(getApplicationContext(), ""+distance, Toast.LENGTH_LONG).show();
                     moveCamera(llOrigem,DEFAULT_ZOOM, adOrigem.getAddressLine(0));
-
+                    Log.e("DISTANCIA", "ANTES DE CHAMAR O MATRIX");
+                    double kms = getDistanceInfo(llOrigem, llDestino);
+                    Log.e("DISTANCIA", "DEPOIS DE CHAMAR O MATRIX");
+                    /*mMap.addPolyline(new PolylineOptions().add(llOrigem, new LatLng(llOrigem.latitude,llOrigem.longitude),new LatLng(llDestino.latitude,llDestino.longitude), llDestino))
+                    .setWidth(10);
+                    */
                 } catch (Exception e) {
                     Log.e(TAG, "CATCH DO GEOCODER");
                 }
@@ -94,7 +156,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+
+        MarkerOptions origem = new MarkerOptions();
+        MarkerOptions destino = new MarkerOptions();
     }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -102,6 +168,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Log.e("CRIADO", "DEPOIS DO CONTENT");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_maps);
+
         /*
         setContentView(R.layout.layout_maps);
         //Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -118,12 +185,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(MapsActivity.this);
     }
 
+
     private void moveCamera(LatLng latLng, float zoom, String title){
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
         MarkerOptions options = new MarkerOptions().position(latLng).title(title);
         mMap.addMarker(options);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
 
